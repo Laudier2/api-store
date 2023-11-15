058ed3668b8dde763a66d7814e6f8450b41c1af1
Laudier st <55962333+Laudier2@users.noreply.github.com>
2023-11-14 21:00:46 -0300

    Delete dist directory
    Não necessario

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const port = process.env.PORT || 3001;
-index_1.app.listen(port, () => console.log(`Server na URL ${port}`));
