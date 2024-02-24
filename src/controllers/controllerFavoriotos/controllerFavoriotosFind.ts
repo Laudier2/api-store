b703aafc94a42c3b3a01d533dfc807f9765c44fb
Laudier2 <trabalhandoline2019@hotmail.com>
2024-02-24 02:15:01 -0300

    update

--- src/controllers/controllerFavoriotos/controllerFavoriotosFind.ts
+++ src/controllers/controllerFavoriotos/controllerFavoriotosFind.ts
@@ -4,7 +4,7 @@ import { prisma } from "../../prisma_Client_Orm/prismaClient";
 export class ControllerFavoritFind {
   async handle(request: Request, response: Response) {
 
-    const user = await prisma.favorito.findMany({})
+    const user = await prisma.favorito.findFirst({})
 
     return response.status(200).json(user)
 
