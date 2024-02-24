d2bc5937d17442e41765135fb1209c6c901e7929
Laudier2 <trabalhandoline2019@hotmail.com>
2024-02-24 02:40:13 -0300

    update

--- src/controllers/controllerComentarios/controllerCompraFind.ts
+++ src/controllers/controllerComentarios/controllerCompraFind.ts
@@ -3,8 +3,20 @@ import { prisma } from "../../prisma_Client_Orm/prismaClient";
 
 export class ControllerComentarioFind {
   async handle(request: Request, response: Response) {
+    const id = request.body;
 
-    const user = await prisma.comentarios.findMany({})
+    const user = await prisma.comentarios.findMany({
+      where: {
+        id: id
+      },
+      include: {
+        comentariorealations: {
+          include: {
+            product: true
+          }
+        }
+      }
+    })
 
     return response.status(200).json(user)
 
